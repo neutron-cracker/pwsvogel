075e0e107e28fc1c5b955f578b4e10096c888e19
André Vis <andre.vis@outlook.com>
2020-11-21 16:53:23 +0100

    make transform data function

--- ann.py
+++ ann.py
@@ -1,7 +1,8 @@
 import os
 import pandas as pd
 import tensorflow as tf
-from data import Data_processing as dp
+import data
+from data import Data_processing as dp, transform_data_frame
 from model import Create_model as cm
 # main functions
 
@@ -11,15 +12,13 @@ def get_data(path_to_csv):
     raw_weather_dataframe = dp.process_time_data(raw_dataframe)
     clean_dataframe = dp.process_weather_data(
         raw_weather_dataframe)
-    dataset = dp.convert_pandas_dataframe_to_tf_dataset(clean_dataframe)
-    del raw_dataframe
-    del raw_weather_dataframe
-    # del clean_dataframe
+    transformed_data_frame = data.transform_data_frame.transform_data(clean_dataframe)
+    dataset = dp.convert_pandas_dataframe_to_tf_dataset(transformed_data_frame)
     return dataset
 
 
 def build_model():
-    model = Create_model
+    input_shape = (30)
     return model
 
 
@@ -33,6 +32,8 @@ def predict_with_model(model, data):
     return predicted_data
     pass
 
+b = -1
+days_back = -1 * b
 
 path_to_csv = os.path.join(os.getcwd(), "data/weather_data.csv")
 weather_data = get_data(path_to_csv)
