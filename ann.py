e40d34f7d816c635189c1afb602919250e476327
André Vis <52654095+Hard-Biter@users.noreply.github.com>
2020-12-15 13:31:32 +0100

    Update ann.py

--- ann.py
+++ ann.py
@@ -1,29 +1,26 @@
 import os
-
-from pandas.core.frame import DataFrame
-from six import print_
-import data
-import pandas
-from data import Convert_data
-from data import Get_data
-from model import Model as modelClass
 import tensorflow
 import numpy
 import matplotlib.pyplot as plt
+import data
+import pandas
+from os import path, getcwd()
+from pandas.core.frame import DataFrame
+from six import print_
+from data import Convert_data, Get_data
+from model import Model as modelClass
 from tensorflow import keras
-# main functions
-
 # --------------------------------------------------------------------------------------------------------
 # Read CSV file for weather and parse dates into pandas dataframe.
 # --------------------------------------------------------------------------------------------------------
 
-root_path = os.getcwd()
-path_to_csv_weather = os.path.join(root_path, "data/weather_data.csv")
-path_to_csv_bird = os.path.join(
+root_path = getcwd()
+path_to_csv_weather = path.join(root_path, "data/weather_data.csv")
+path_to_csv_bird = path.join(
     root_path, "data/bird_migration_per_specie.csv")
 
-weather_data_raw = data.Get_data.raw_weather_data(path_to_csv_weather)
-bird_data_unfiltered = data.Get_data.bird_data(path_to_csv_bird)
+weather_data_raw = Get_data.raw_weather_data(path_to_csv_weather)
+bird_data_unfiltered = Get_data.bird_data(path_to_csv_bird)
 
 
 weather_data_raw = Get_data.transform_weather_data(weather_data_raw)
@@ -44,42 +41,14 @@ model = modelClass.build_model()
 # Get number of rows and calculate the amount needed for respectively train, validation, and test data.
 # Define model, set target_data and pecify the amount of epochs.
 # --------------------------------------------------------------------------------------------------------
+train_weather_dataframe = Get_data.part_of_data(weather_data, train)
+validation_weather_dataframe = Get_data.part_of_data(weather_data, validation)
+test_weather_dataframe = Get_data.part_of_data(weather_data, test)
 
-length_dataframe = len(weather_data)  # count numbers of rows in csv file
+train_bird_dataframe = Get_data.part_of_data(bird_data, train)
+validation_bird_dataframe = Get_data.part_of_data(bird_data, validation)
+test_bird_dataframe = Get_data.part_of_data(bird_data, test)
 
-# takes 70% of that number of rows
-train_weather_dataframe = weather_data[0:int(0.7*length_dataframe)]
-# takes the next 20% of that number of rows
-validation_weather_dataframe = weather_data[int(
-    0.7*length_dataframe):int(0.9*length_dataframe)]
-# takes the last 10% of that number of rows
-test_weather_dataframe = weather_data[int(
-    0.9*length_dataframe):length_dataframe]
-
-train_bird_dataframe = bird_data[0:int(0.7*length_dataframe)]
-validation_bird_dataframe = bird_data[int(
-    0.7*length_dataframe):int(0.9*length_dataframe)]
-test_bird_dataframe = bird_data[int(0.9*length_dataframe):length_dataframe]
-
-
-# target_data = Data_processing.process_bird_migration_data(bird_data)  # TODO
-# train_dataset = Convert_data.dataframe_to_tf_dataset(
-#     train_weather_dataframe, train_bird_dataframe)  # , train_dataframe)
-epochs = 200
-
-# export dataframe
-
-
-Get_data.exportFile(train_bird_dataframe, "bird.csv")
-Get_data.exportFile(train_weather_dataframe, "weather.csv")
-
-
-# --------------------------------------------------------------------------------------------------------
-# Train model and save the model.
-# --------------------------------------------------------------------------------------------------------
-
-print(len(train_bird_dataframe.columns))
-print(train_bird_dataframe.columns[38])
 for column in range((len(train_bird_dataframe.columns) - 1), 10, -1):
     train_bird_dataframe = train_bird_dataframe.drop(
         train_bird_dataframe.columns[column], axis=1)
@@ -90,8 +59,16 @@ for column in range((len(test_bird_dataframe.columns) - 1), 10, -1):
     test_bird_dataframe = test_bird_dataframe.drop(
         test_bird_dataframe.columns[column], axis=1)
 
+# export dataframe
 
-print(validation_bird_dataframe.shape)
+Get_data.exportFile(train_bird_dataframe, "bird.csv")
+Get_data.exportFile(train_weather_dataframe, "weather.csv")
+
+# --------------------------------------------------------------------------------------------------------
+# Train model and save the model.
+# --------------------------------------------------------------------------------------------------------
+   
+epochs = 200
 
 history = model.fit(x=train_weather_dataframe, y=train_bird_dataframe, epochs=epochs, verbose=2, shuffle=True,
                     validation_data=(validation_weather_dataframe, validation_bird_dataframe))
@@ -108,12 +85,11 @@ plt.ylabel('Loss')
 plt.savefig('loss.png')
 plt.show()
 
-model.save(os.path.join(root_path, "pwsvogelmodel"))
+model.save(path.join(root_path, "pwsvogelmodel"))
 predictions = model.predict(x=test_weather_dataframe)
-predictions = pandas.DataFrame(data=predictions)
+predictions = DataFrame(data=predictions)
 Get_data.exportFile(predictions, "test_predictions.csv")
 
-path = os.path.join(os.getcwd(), 'pwsvogelmodel')
-model = keras.models.load_model(path)
-validation_loss = model.evaluate(x=test_weather_dataframe, y=test_bird_dataframe)
-print(validation_loss)
+path = path.join(root_path, 'pwsvogelmodel')
+model = models.load_model(path)
+test_loss = model.evaluate(x=test_weather_dataframe, y=test_bird_dataframe)
